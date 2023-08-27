a0a422f2a7f584619085dde9b0e8f89b20036ad1
SharukhGV <sharukhvelupillai@pursuit.org>
2023-08-27 04:39:20 -0400

    integration user auth middleware posts

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,11 @@
--- \c craftopia_database; 
+\c craftopia_database; 
 
+INSERT INTO user_cred (email, password) VALUES ('hellometropcs@gmail.com','user');
 
+INSERT INTO user_profile (username, profile_pic, birth_date, locale_home, learning_interest, current_skillset, userid) VALUES ('sharukhgv','null','1995-01-29', '-74.006000, 40.712800', 'painting', 'drawing', 1);
 
--- -- Inserting user profiles
--- INSERT INTO user_profiles (username, email) VALUES ('user1', 'user1@example.com');
--- INSERT INTO user_profiles (username, email) VALUES ('user2', 'user2@example.com');
+ -- -- Inserting posts
+INSERT INTO posts (title, tags, body, created_at, edited_at, userid) VALUES ('How to Paint', 'paint', 'mountain view','20230423 11:42:35.173', null, 1);
 
+INSERT INTO posts (title, tags, body, created_at, edited_at, userid) VALUES ('How to draw', 'draw', 'mountain view','20230423 11:42:35.173', null, 1);
 
--- queries/posts.js
+++ queries/posts.js
@@ -1,52 +1,129 @@
 const db = require("../db/dbConfig.js");
+const { findAccount } = require("./auth.js");
 
+//function will get all post from a single user given the email to find the userid and hence ensuring user authenticated before posts are brought up for CRUD actions
+/**
+ * gets all posts from a user
+ * @param {string} email - email associated with a user_cred account
+ * @returns {object} all posts via json format
+ **/
+const getAllPostsFromUser = async (email) => {
+  const account = await findAccount(email);
 
-const getAllPostsFromUser = async () => await db.any("SELECT * FROM posts WHERE profile_id=$1", profile_id);
-
-//ALL POSTS FROM ALL USERS EDIT THIS
-const getAllPosts = async () => await db.any("SELECT * FROM posts")
-
-
-const getOnePost = async (post_id) =>
-  await db.one("SELECT * FROM posts WHERE user_id=$1, post_id=$2",[post_id]);
-
-
-const updateOnePost = async (post_id,posts) => {
-  const { title, tags, body, created_at, edited_at} = posts;
-
-  return await db.one(
-    "UPDATE posts SET title=$1, tags=$2, body=$3, created_at=$4, edited_at=$5, WHERE post_id=$6 RETURNING *",
-    [title, tags, body, created_at, edited_at, post_id]
-  );
+  if ("userid" in account) {
+    const userid = account.userid;
+    const post = await db.any("SELECT * FROM posts WHERE userid=$1", userid);
+    return post;
+  } else {
+    return { message: "No account found with that email" };
+  }
 };
 
-const deletePost = async (post_id) =>
-  await db.one("DELETE FROM posts WHERE post_id=$1 RETURNING *", post_id);
-  
+//function will get a single post from a single user given the email to find the userid and hence ensuring user authenticated before the post is brought up retrieval
+/**
+ * gets a single post from a user based on email and post identification
+ * @param {string} email - email associated with a user_cred account
+ * @param {number} post_id - a specific post's identification number
+ * @returns {object} a single post via json format
+ **/
+const getOnePost = async (email, post_id) => {
+  const account = await findAccount(email);
 
-  const createPosts = async (posts) => {
+  if ("userid" in account) {
+    const userid = account.userid;
+    const post = await db.one(
+      "SELECT * FROM posts WHERE userid=$1 AND post_id=$2",
+      [userid, post_id]
+    );
+    return post;
+  } else {
+    return { message: "No account found with that email" };
+  }
+};
 
-    const insertedPosts = await db.one(
-      "INSERT INTO posts (title, tags, body, created_at, edited_at) VALUES($1, $2, $3, $4, $5) RETURNING *",
+//function will update a post from a single user given the email to find the userid and hence ensuring user authenticated before posts are brought up for updating action
+/**
+ * gets a single post from a user based on email and post identification
+ * @param {string} email - email associated with a user_cred account
+ * @param {number} post_id - a specific post's identification number
+ * @param {object} posts - the post's data to be edited
+ * @returns {object} a single post via json format that has been edited
+ */
+const updateOnePost = async (email, post_id, posts) => {
+  const account = await findAccount(email);
+
+  if ("userid" in account) {
+    const userid = account.userid;
+    const { title, tags, body, created_at, edited_at } = posts;
+    const post = db.one(
+      "UPDATE posts SET title=$1, tags=$2, body=$3, created_at=$4, edited_at=$5, WHERE  userid=$6, post_id=$7 RETURNING *",
+      [title, tags, body, created_at, edited_at, userid, post_id]
+    );
+    return post;
+  } else {
+    return { message: "No account found with that email" };
+  }
+};
+
+//function will get a post from a single user given the email to find the userid and hence ensuring user authenticated before posts are brought up for deletion
+/**
+ * deletes a single post from a user based on email and post identification
+ * @param {string} email - email associated with a user_cred account
+ * @param {number} post_id - a specific post's identification number
+ **/
+const deletePost = async (email, post_id) => {
+  const account = await findAccount(email);
+
+  if ("userid" in account) {
+    const userid = account.userid;
+    const post = await db.one(
+      "DELETE FROM posts WHERE userid=$1, post_id=$2 RETURNING *",
+      [userid, post_id]
+    );
+  } else {
+    return { message: "No account found with that email" };
+  }
+};
+
+/**
+ * creates a single post from a user based on email which will determine the userid and thus the user who is making the post
+ * @param {string} email - email associated with a user_cred account
+ * @param {number} post_id - a specific post's identification number
+ * @param {object} posts - the post's data to be edited
+ * @returns {object} a single post via json format
+ */
+const createPosts = async (email) => {
+  const account = await findAccount(email);
+
+  if ("userid" in account) {
+    const userid = account.userid;
+    const post = await db.one(
+      "INSERT INTO posts (title, tags, body, created_at, edited_at) VALUES($1, $2, $3, $4, $5), WHERE userid=$6 RETURNING *",
       [
-        posts.title,
-        posts.tags,
-        posts.body,
-        posts.created_at,
-        posts.edited_at,
-        posts.date,
-        posts.night,
+        post.title,
+        post.tags,
+        post.body,
+        post.created_at,
+        post.edited_at,
+        userid,
       ]
     );
-  
-    return insertedPosts;
-  }
 
+    return post;
+  }
+};
+//scrap code below for maybe other purpose
+// //ALL POSTS FROM ALL USERS EDIT THIS
+// const getAllPosts = async () => await db.any("SELECT * FROM posts")
+
+// const getOnePost = async (userid, post_id) =>
+//   await db.one("SELECT * FROM posts WHERE userid=$1, post_id=$2",[userid, post_id]);
+//scrap code above for maybe other purpose
 
 
 module.exports = {
   getAllPostsFromUser,
-  getAllPosts,
+  // getAllPosts,
   getOnePost,
   updateOnePost,
   deletePost,
