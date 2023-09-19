2d2f7711a652a8c69715d2b03276c41eecad5cbe
Felizj17 <felizj17@gmail.com>
2023-09-19 01:47:53 -0400

    seed file for barebones posts and tools

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const posts = express.Router()
-const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
+const express = require('express');
+const posts = express.Router();
+const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3');
 const {
   getAllPostsFromUser,
   getAllPosts,
@@ -10,113 +10,116 @@ const {
   createPosts,
   postMedia,
   addThumbnail,
-} = require('../queries/posts')
-const s3 = new S3Client()
+} = require('../queries/posts');
+const s3 = new S3Client();
 
 //all posts from specific user
 posts.get('/:id', async (req, res) => {
-  const {id} = req.params
+  const {id} = req.params;
   try {
-    const posts = await getAllPostsFromUser(id)
-    res.json(posts)
+    const posts = await getAllPostsFromUser(id);
+    res.json(posts);
   } catch (error) {
-    console.log(error)
-    res.status(400).json({error: 'something went terribly wrong!'})
+    console.log(error);
+    res.status(400).json({error: 'something went terribly wrong!'});
   }
-})
+});
 
 //all posts from all users
 posts.get('/', async (req, res) => {
   try {
-    const posts = await getAllPosts()
-    res.json(posts)
+    const posts = await getAllPosts();
+    res.json(posts);
   } catch (error) {
-    console.log(error)
-    res.status(400).json({error: 'something went terribly wrong!'})
+    console.log(error);
+    res.status(400).json({error: 'something went terribly wrong!'});
   }
-})
+});
 
 posts.get('/:id', async (req, res) => {
   try {
-    const {id} = req.params
-    const post = await getOnePost(id)
-    res.json(post)
+    const {id} = req.params;
+    const post = await getOnePost(id);
+    res.json(post);
   } catch (error) {
-    console.log(error)
-    res.status(404).json({error: 'That post log does not exist!'})
+    console.log(error);
+    res.status(404).json({error: 'That post log does not exist!'});
   }
-})
+});
 
 posts.put('/:id', async (req, res) => {
   try {
-    const {id} = req.params
-    const post = req.body
+    const {id} = req.params;
+    const post = req.body;
 
-    const updatedpost = await updateOnePost(id, post)
-    res.json(updatedpost)
+    const updatedpost = await updateOnePost(id, post);
+    res.json(updatedpost);
   } catch (error) {
-    console.log(error)
-    res.status(400).json({error: 'Cannot update post log'})
+    console.log(error);
+    res.status(400).json({error: 'Cannot update post log'});
   }
-})
+});
 
 posts.delete('/:id', async (req, res) => {
   try {
-    const {id} = req.params
-    const deletedpost = await deletePost(id)
-    res.json(deletedpost)
+    const {id} = req.params;
+    const deletedpost = await deletePost(id);
+    res.json(deletedpost);
   } catch (error) {
-    console.log(error)
-    res.status(400).json({error: 'Catastrophe! Something went terribly wrong!'})
+    console.log(error);
+    res
+      .status(400)
+      .json({error: 'Catastrophe! Something went terribly wrong!'});
   }
-})
+});
 
 posts.post('/', async (req, res) => {
-  const files = []
-  const fileKeys = req.files!==null? Object.keys(req.files): []
-
+  const fileKeys = req.files!==null?Object.keys(req.files):[];
+  const files = [];
   fileKeys.forEach(key => {
-    files.push(req.files[key])
-  })
+    files.push(req.files[key]);
+  });
   try {
-    const post = req.body
-    const createdPost = await createPosts(post)
+    const post = req.body;
+    const createdPost = await createPosts(post);
     if (!createdPost.error) {
       files.forEach(async (file, i) => {
-        console.log(file)
+        console.log(file);
         if (i === 0) {
-          uploadImageS3(file, `${createdPost.post_id}_thumbnail`)
+          uploadImageS3(file, `post_${createdPost.post_id}_thumbnail`);
           await addThumbnail(
-            `${process.env.CLOUDFRONT_URI}/${createdPost.post_id}_thumbnail${i}`,
+            `${process.env.CLOUDFRONT_URI}/post_${createdPost.post_id}_thumbnail${i}`,
             createdPost.post_id
-          )
+          );
         } else {
-          uploadImageS3(file, `${createdPost.post_id}_image${i}`)
+          uploadImageS3(file, `post_${createdPost.post_id}_image${i}`);
           uploadImageDb(
             file,
-            `${createdPost.post_id}_image${i}`,
+            `post_${createdPost.post_id}_image${i}`,
             createdPost.post_id
-          )
+          );
         }
-      })
+      });
     }
-    res.status(200).json({message: 'Post Successful', createdPost: createdPost})
+    res
+      .status(200)
+      .json({message: 'Post Successful', createdPost: createdPost});
   } catch (error) {
-    console.log(error)
-    console.log('Incoming request body:', req.body)
-    res.status(400).json({error: 'Incorrect post body'})
+    console.log(error);
+    console.log('Incoming request body:', req.body);
+    res.status(400).json({error: 'Incorrect post body'});
   }
-})
+});
 
 const uploadImageS3 = async (file, imageName, post_id) => {
   const params = {
     Bucket: process.env.BUCKET_NAME,
     Key: imageName,
     Body: file.data,
-  }
+  };
 
   try {
-    const results = await s3.send(new PutObjectCommand(params))
+    const results = await s3.send(new PutObjectCommand(params));
     console.log(
       'Successfully created ' +
         params.Key +
@@ -124,13 +127,13 @@ const uploadImageS3 = async (file, imageName, post_id) => {
         params.Bucket +
         '/' +
         params.Key
-    )
+    );
 
-    return results // For unit tests.
+    return results; // For unit tests.
   } catch (err) {
-    console.log('Error:', err)
+    console.log('Error:', err);
   }
-}
+};
 const uploadImageDb = async (file, imageName, post_id) => {
   const dbParams = {
     file_name: imageName,
@@ -138,12 +141,12 @@ const uploadImageDb = async (file, imageName, post_id) => {
     file_type: file.mimetype,
     file_url: `${process.env.CLOUDFRONT_URI}${imageName}`,
     post_id: post_id,
-  }
-  const dbResults = await postMedia(dbParams)
+  };
+  const dbResults = await postMedia(dbParams);
   if (!dbResults.error) {
-    return dbResults
+    return dbResults;
   } else {
-    res.status(400).json({error: dbResults.error})
+    res.status(400).json({error: dbResults.error});
   }
-}
-module.exports = posts
+};
+module.exports = posts;
--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,97 @@
-\c craftopia_database; 
+\ c craftopia_database;
 
 -- INSERT INTO user_cred (email, password) VALUES ('hellometropcs@gmail.com','user');
+INSERT INTO
+    posts (title, category, body, user_id)
+VALUES
+    (
+        'Hey I just learned how to knit!',
+        'Knitting',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        1
+    ),
+    (
+        'Hey I just learned how to Crochet!',
+        'Crocheting',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        1
+    ),
+    (
+        'Hey I just learned how to paint!',
+        'Paint',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        2
+    ),
+    (
+        'Hey I just learned how to Draw!',
+        'Sketch',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        2
+    ),
+    (
+        'Hey I just learned how to take a good Photo!',
+        'Photography',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        3
+    ),
+    (
+        'Hey I just learned about filmmaking!',
+        'Filmmaking',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        3
+    ),
+    (
+        'Hey I just learned how to Graffiti!',
+        'Graffiti',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        4
+    ),
+    (
+        'Hey I just learned how to Sculpt!',
+        'Sculpt',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        4
+    );
 
-INSERT INTO users (user_id, name, email, password, username, dob, city_state, profile_pic, learning_interest, current_skillset) VALUES (1, 'sharukhgv','sharukhgv@email.com', 'SVpassword', 'sharukhUSERNAME', '1995-01-29', 'Sharukh, NY', '-74.006000, 40.712800', 'painting', 'drawing'),
-(3, 'evanv', 'evanv@email.com', 'EVpassword', 'evanUSERNAME', '1986-02-10', 'Evan, CT', '-74.006000, 40.712800', 'sculpting', 'photography');
-
-INSERT INTO posts (title, tags, body, created_at, edited_at, user_id) VALUES ('How to Paint', 'paint', 'mountain view','20230423 11:42:35.173', null, 1);
-
-INSERT INTO posts (title, tags, body, created_at, edited_at, user_id) VALUES ('How to draw', 'draw', 'mountain view','20230423 11:42:35.173', null, 1);
-
-
-INSERT INTO hobby (name_hobby) VALUES ('Painting');
-
-INSERT INTO hobby (name_hobby) VALUES ('Pottery');
-
-INSERT INTO hobby (name_hobby) VALUES ('Drawing');
+INSERT INTO
+    tools (
+        name,
+        description,
+        price,
+        stock,
+        condition,
+        user_id
+    )
+VALUES
+    (
+        'Paint Brush Set',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        6.99,
+        2,
+        'new',
+        1
+    ),
+    (
+        'Canon camera lense extender RF1.4x  ',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        269.99,
+        1,
+        'good',
+        2
+    ),
+(
+        '600ft assort yarn',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        4.99,
+        2,
+        'new',
+        3
+    ),
+(
+        'Spray Paint Black, Red, Blue, Yellow',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        43.95,
+        2,
+        'new',
+        4
+    ),
