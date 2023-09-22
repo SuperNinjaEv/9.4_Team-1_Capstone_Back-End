24549f8a5aabbe00a6f595cea99d54d6b099b266
SharukhGV <sharukhvelupillai@pursuit.org>
2023-09-22 16:13:24 -0400

    minor fix seed file

--- db/seed.sql
+++ db/seed.sql
@@ -269,8 +269,8 @@ VALUES
         'Crochet fashion: Create your own stylish accessories!',
         'Crocheting',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'fashioncrochet',
-        6
+        'sharukh',
+        4
     ),
     (
         'Crochet challenges: Lets tackle them head-on!',
